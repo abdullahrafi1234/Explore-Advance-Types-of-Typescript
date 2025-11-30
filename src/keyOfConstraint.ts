2c0eef6e1a3557cbdab492e744b532bb154976f3
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-11-30 23:00:57 +0600

    deep discussion key of constraint

--- src/keyOfConstraint.ts
+++ src/keyOfConstraint.ts
@@ -11,8 +11,17 @@ type MyVehicle2 = keyof RichPeoplesVehicles;
 
 const myVehicle1: MyVehicle1 = "cng";
 const myVehicle2: MyVehicle2 = "car";
+// 1. This method is dynamic. If you add `plane: string` to the RichPeoplesVehicles type in the future, `MyVehicle2` will automatically include `"plane"`. But you will have to
 
-const user = {
+type User = {
+  id: number;
+  name: string;
+  address: {
+    city: string;
+  };
+};
+
+const user: User = {
   id: 222,
   name: "Rafi",
   address: {
@@ -22,5 +31,30 @@ const user = {
 
 // const myId = user.id  // dot notation
 const myId = user["id"]; // bracket notation
+const myName = user["name"];
+const myAddress = user["address"];
 
-// const getPropertyFromObject = (obj: object, key: string)
+// console.log({ myId, myName, myAddress });
+
+// const getPropertyFromObject = (obj: User, key: "name"| "id" |"address") => {
+//   return obj[key];
+// };
+
+// const getPropertyFromObjectKeyof = (obj: User, key: keyof User) => {
+//   return obj[key];
+// };
+const getPropertyFromObject = <X>(obj: X, key: keyof X) => {
+  return obj[key];
+};
+
+// const result = getPropertyFromObject(user, "name");
+// console.log(result);
+// const result2 = getPropertyFromObject(user, "name");
+// console.log(result2);
+
+const product = {
+  brand: "HP",
+};
+
+const result3 = getPropertyFromObject(product, "brand");
+console.log(result3);
