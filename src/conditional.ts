5b893e95c4d06db8b9ab36299c0177d2fafd9b4e
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-01 13:15:01 +0600

    conditional type discussion

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,1 +1,19 @@
 // conditional type :
+
+type A = null;
+type B = undefined;
+
+type C = A extends number ? true : B extends undefined ? true : false;
+
+type Vehicles = {
+  bike: string;
+  car: string;
+  ship: string;
+};
+
+// type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? true : false;
+
+// Using keyof operator
+type CheckVehicle<T> = T extends keyof Vehicles ? true : false;
+
+type HasBike = CheckVehicle<"cycle">;
