7cf7b7e4576111500cde574874d6efa75e8af46d
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-12-02 12:41:58 +0600

    add more utility type

--- src/utility.ts
+++ src/utility.ts
@@ -39,3 +39,27 @@ type ReadOnlyProducts = Readonly<Product>;
 // empty object ( it help us to understand that the key is string)
 
 const emptyObject: Record<string, unknown> = {};
+
+// Exclude<T, U>
+type Letters = "A" | "B" | "C";
+type ExcludedLetters = Exclude<Letters, "A">; // "B" | "C"
+
+// Extract<T, U>
+type Numbers = 1 | 2 | 3;
+type ExtractedNumbers = Extract<Numbers, 2 | 3 | 4>; // 2 | 3
+
+// NonNullable<T>
+type NullableType = string | null | undefined;
+type NonNullableType = NonNullable<NullableType>; // string
+
+// ReturnType<T>
+function getUser() {
+  return { name: "Karim", age: 28 };
+}
+type UserType = ReturnType<typeof getUser>;
+
+// Parameters<T>
+function sum(a: number, b: number): number {
+  return a + b;
+}
+type SumParameters = Parameters<typeof sum>; // [number, number]
